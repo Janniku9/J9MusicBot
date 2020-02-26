265a50663aa2fbcad98c55cf54df6968ff0528a3
Janniku9 <gartmann.jannik@gmail.com>
2020-02-26 21:18:45 +0100

    starting with the bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,11 @@
 import {DataBaseHelper} from "./db";
+import TelegramBot from 'node-telegram-bot-api';
 import {TOKEN, CHANNEL} from "./const";
 
-
 const db:DataBaseHelper = new DataBaseHelper();
+
+const bot : TelegramBot = new TelegramBot(TOKEN, {polling: true});
+
+bot.onText(/\/myid/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Your ID: ${msg.from?.id}`);
+});
