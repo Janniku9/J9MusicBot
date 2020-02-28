ffe143cb3fc2b40492acafc9952587fd3f4f722e
Janniku9 <gartmann.jannik@gmail.com>
2020-02-28 16:08:35 +0100

    genre keyboard + submission menu

--- src/bot.ts
+++ src/bot.ts
@@ -102,6 +102,43 @@ bot.onText(/\/moderators/, (msg) => {
 
 // SUBMISSION STUFF
 
+function submission_menu (song_id: number) :any {
+    const song = db.get_song(song_id);
+    const t = song.title == "" ? "add title":"edit title";
+    const a = song.artists == [] ? "add artist":"edit artists";
+    const g = song.genres == [] ? "add genres":"edit genres";
+
+    return  JSON.stringify({
+        inline_keyboard: [[
+            {text: t, callback_data: song_id + "-submission-menu-title"},
+            {text: a, callback_data: song_id + "-submission-menu-artists"},
+            {text: g, callback_data: song_id + "-submission-menu-genres"}
+        ], [
+            {text: "submit", callback_data: song_id + "-submission-menu-submit"},
+            {text: "cancel", callback_data: song_id + "-submission-menu-cancel"}
+        ]]
+    });
+}
+
+function handle_submission_menu_callback(action, callback_query) {
+    bot.answerCallbackQuery(callback_query.id);
+    const chat_id = callback_query.message.chat.id;
+    const msg_id = callback_query.message.message_id;
+    const song_id = action.split("-")[0];
+
+    if (action.includes("submit")) {
+        bot.sendMessage(chat_id, "Submitted. TODO")
+    } else if (action.includes("cancel")) {
+        bot.sendMessage(chat_id, "cancelled. TODO")
+    } else if (action.includes("title")) {
+        bot.sendMessage(chat_id, "title. TODO")
+    } else if (action.includes("artists")) {
+        bot.sendMessage(chat_id, "artists. TODO")
+    } else if (action.includes("genres")) {
+        bot.editMessageReplyMarkup(genre_keyboard(song_id, 0), {chat_id: chat_id, message_id: msg_id})
+    }
+}
+
 function post_menu (song_id: number) : any {
     const song = db.get_song(song_id);
     const ratings = ['🗑️', '👎', '👌', '👍', '🔥']
@@ -118,6 +155,10 @@ function post_menu (song_id: number) : any {
         });
 }
 
+function handle_score_callback (action: string, callback_query) {
+
+}
+
 function submission_text(song_id: number) : string {
     const song:Song = db.get_song(song_id);
     return ("<b>" + song.title + "</b>" +
@@ -126,9 +167,58 @@ function submission_text(song_id: number) : string {
       "\n\n<b><i>Url:</i></b>         " + song.url);
 }
 
+function genre_keyboard(song_id: number, page: number) : any {
+    const used_genres = db.get_song(song_id).genres;
+    const genres = db.get_genres();
+    const total_pages: number = Math.ceil(genres.length/12.0);
+    const left_page: number = (page -1 + total_pages) % total_pages;
+    const right_page: number = (page + 1) % total_pages;
+    
+    const left_bound = 12 * page;
+    const right_bound = Math.min(12 * (page+1), genres.length);
+    const p = genres.slice(left_bound, right_bound).map(g => ({text: (used_genres.find(a => a == g) == undefined ? "": "✅ ") + "#" + g, callback_data: song_id + "-" + g + "-genre-page-selection"}))
+    return JSON.stringify({
+        inline_keyboard: [
+            p.slice(0,3), p.slice(3,6), p.slice(6,9), p.slice(9,12) 
+        ,[
+        {
+            text: "<",
+            callback_data: song_id + "-" + left_page + "-genre-page"
+        },
+        {
+            text: "done!",
+            callback_data: song_id + "-done-genre-page"
+        },
+        {
+            text: ">",
+            callback_data: song_id + "-" + right_page + "-genre-page"
+        }
+        ]]
+    });
+}
+
+function handle_genre_page_selection_callback (action:string, callback_query) {
+
+}
+
+function handle_genre_page_switch_callback (action:string, callback_query) {
+    bot.answerCallbackQuery(callback_query.id);
+    const chat_id = callback_query.message.chat.id;
+    const msg_id = callback_query.message.message_id;
+
+    if (action.includes('done-genre-page')) {
+        const song_id = parseInt(action.split('-')[0]);
+        bot.editMessageReplyMarkup(submission_menu(song_id), {chat_id: chat_id, message_id: msg_id})
+    } else {
+        const song_id = parseInt(action.split('-')[0]);
+        const page = parseInt(action.split('-')[1]);
+        bot.editMessageReplyMarkup(genre_keyboard(song_id, page), {chat_id: chat_id, message_id: msg_id})
+    }
+}
+
 bot.onText(/\/submit/, (msg) => {
     const song_id = 1;
-    bot.sendMessage(msg.chat.id, submission_text(song_id), {parse_mode: 'HTML', reply_markup: post_menu(song_id)});
+    bot.sendMessage(msg.chat.id, submission_text(song_id), {parse_mode: 'HTML', reply_markup: submission_menu(song_id)});
 });
 
 bot.onText(/\/my_submissions/, (msg) => {
@@ -148,3 +238,22 @@ bot.onText(/\/top/, (msg) => {
 bot.onText(/\/feedback/, (msg) => {
     bot.sendMessage(msg.chat.id, "TODO", {parse_mode: 'HTML'});
 });
+
+// CALLBACKS
+
+bot.on('callback_query', function onCallbackQuery(callback_query) {
+    const action = callback_query.data;
+
+    if (action.includes('-genre-page-selection'))
+        handle_genre_page_selection_callback(action, callback_query);
+    else if (action.includes('-genre-page'))
+        handle_genre_page_switch_callback(action, callback_query);
+    else if (action.includes('-score'))
+        handle_score_callback(action, callback_query);
+    else if (action.includes('-submission-menu'))
+        handle_submission_menu_callback(action, callback_query);
+  });
+
+// ERROR
+
+bot.on('polling_error', error => console.log(error))
